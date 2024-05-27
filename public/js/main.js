07c5dc01384a5ca69537319d85ecff6597f05ee3
Allister Sascha <101225242+allistersascha@users.noreply.github.com>
2024-05-27 14:36:32 -0500

    mostly broken sowwy

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,38 @@ const audio = document.querySelector("audio");
 const durationCont = document.getElementById("duration");
 const currentTimeCont = document.getElementById("currentTime");
 const outputCont = document.getElementById("volumeOutput");
+
+let playState = "play";
+let muteState = "unmute";
+/* vs code HATES all of these but why???
+function togglePlayPause(){
+    if (playState === "play"){
+        i class = "fa-solid fa-pause";
+        playState = "pause";
+    }else{
+        i class = "fa-solid fa-play";
+        playState = "play";
+    }
+}
+
+function toggleMute(){
+    if (muteState === "unmute"){
+        i class = "fa-solid fa-volume";
+        muteState = "mute";
+    }else{
+        i class = "fa-solid fa-volume-xmark";
+        muteState = "unmute";
+    }
+} */
+
+const showRangeProgress = (rangeInput) => {
+    if(rangeInput === seekSlider) audioPlayerCont.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+    else audioPlayerCont.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+}
+
+seekSlider.addEventListener('input', (e) => {
+    showRangeProgress(e.target);
+});
+volumeSlider.addEventListener('input', (e) => {
+    showRangeProgress(e.target);
+});
